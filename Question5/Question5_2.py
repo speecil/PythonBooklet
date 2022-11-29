534136a1c85d0a1940fbfef2bc10aec61011d954
speecil <90689870+speecil@users.noreply.github.com>
2022-11-29 15:01:14 +1100

    im stupuid

--- Question5/Question5_2.py
+++ Question5/Question5_2.py
@@ -1,6 +1,6 @@
 sesame = input('What is the passphrase? ')
 sesame = sesame.lower()
-while sesame != 'open SESAME':
+while sesame != 'open sesame':
     print('No. Try again!')
     sesame = input('What is the passphrase? ')
 print('You may enter.')
